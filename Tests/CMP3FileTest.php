748b1fda30c7becc94236d84dc3ff52548a900d2
Daniel <daniel@allgigs.co.uk>
2015-11-17 23:22:46 +0000

    Set up PHPUnit and initial basic test

---
+++ Tests/CMP3FileTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 17/11/15
+ * Time: 23:17
+ */
+namespace oconnedk\id3;
+
+class CMP3FileTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInvalidFileDoesntNeedID3Tags()
+    {
+        $file = new CMP3File("invalid file");
+        $this->assertFalse($file->needsID3Info());
+    }
+} 
---
+++ Tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 17/11/15
+ * Time: 23:11
+ */
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+return $loader;
