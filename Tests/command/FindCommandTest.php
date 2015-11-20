acaf34f8f0170a79ea14f62608627f14d3e40e10
Daniel <daniel@allgigs.co.uk>
2015-11-20 17:02:12 +0000

    Tests: Check for concise option. Check for no path specified. Check for folder and file content in both concise and non-concise cases

--- Tests/command/FindCommandTest.php
+++ Tests/command/FindCommandTest.php
@@ -15,9 +15,24 @@ use Symfony\Component\Console\Tester\CommandTester;
 class FindCommandTest extends \PHPUnit_Framework_TestCase
 {
     const FOUND_FILES_REGEXP = '/These files need ID3 tags.*/';
+    const CONCISE_OPTION = '--concise';
 
     /** @var string */
     protected static $resourcePath = "";
+    /** @var array */
+    private static $defaultArgs = [];
+    /** @var array  */
+    private static $expectedFolders = [
+        "Untagged Artist Name",
+        "MP3 Tests"
+    ];
+    /** @var array  */
+    private static $expectedFiles = [
+        "Artist Name - Track 1.mp3",
+        "Artist Name - Track 2.mp3",
+        "temp.emptytags.mp3",
+        "temp.withtags.mp3"
+    ];
 
     /**
      * @inheritdoc
@@ -25,18 +40,22 @@ class FindCommandTest extends \PHPUnit_Framework_TestCase
     public static function setUpBeforeClass()
     {
         self::$resourcePath = __DIR__."/../Resources";
+        self::$defaultArgs = [
+            'path' => self::$resourcePath
+        ];
     }
 
     /**
-     * Positive test - test that files are found
+     * Negative test - no path
+     * @expectedException \Exception
      */
-    public function testFind()
+    public function testNoPath()
     {
-        $commandTester = self::runFindCommand();
-        $this->assertRegExp(self::FOUND_FILES_REGEXP, $commandTester->getDisplay());
+        self::runFindCommand([]);
     }
 
     /**
+     * Negative test
      * @expectedException \Exception
      */
     public function testFindForInvalidPath()
@@ -45,17 +64,53 @@ class FindCommandTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Positive test - test that files are found
+     */
+    public function testFind()
+    {
+        $commandTester = self::runFindCommand(self::$defaultArgs);
+        $this->assertRegExp(self::FOUND_FILES_REGEXP, $commandTester->getDisplay());
+        // Should find the files under each of those folders
+        $this->assertMatchesThese(self::$expectedFiles, $commandTester);
+    }
+
+    /**
+     * Positive test - test that files are found
+     */
+    public function testFindConcise()
+    {
+        $commandTester = self::runFindCommand(array_merge(self::$defaultArgs, [self::CONCISE_OPTION => true]));
+        $this->assertRegExp(self::FOUND_FILES_REGEXP, $commandTester->getDisplay());
+        // Should find the expected folders
+        $this->assertMatchesThese(self::$expectedFolders, $commandTester);
+    }
+
+    /**
      * @param array $args
      * @return CommandTester
      */
     public static function runFindCommand(array $args = [])
     {
-        $defaultArgs = [
-            'path' => self::$resourcePath
-        ];
         $commandTester = self::createFindTester();
-        $commandTester->execute(array_merge($defaultArgs, $args));
+        $commandTester->execute($args);
         return $commandTester;
+    }
+
+    /**
+     * Check that the supplied filenames (and only those) are contained within the output of the test
+     * @param string[] $fileNames
+     * @param CommandTester $tester
+     * @return int
+     */
+    private function assertMatchesThese(array $fileNames, CommandTester $tester)
+    {
+        $this->assertNotCount(0, $fileNames);   // This would be a programming error on the part of the test writer
+        $display = $tester->getDisplay(true);
+        $matchCount = $display ? substr_count($display, PHP_EOL) - 1 : 0;
+        $this->assertEquals($matchCount, count($fileNames));
+        foreach ($fileNames as $fileName) {
+            $this->assertContains($fileName, $display);
+        }
     }
 
     /**
