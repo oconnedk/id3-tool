149a777ce52ece7753f05d1d2d25b1f81768b5f3
Daniel <daniel@allgigs.co.uk>
2015-11-20 14:29:50 +0000

    Add --concise option for reporting matches (prevents being overwhelmed with responses) and restructure mp3 resources so that they give better reporting results. Refactored tests as a result of file movement

--- src/command/FindCommand.php
+++ src/command/FindCommand.php
@@ -12,6 +12,7 @@ use oconnedk\id3\CMP3File;
 use oconnedk\id3\MediaLocator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
@@ -20,6 +21,7 @@ class FindCommand extends Command
     const COMMAND_NAME = 'find';
     const PATH_ARGUMENT = 'path';
     const FIND_MEDIATYPE = 'mp3';
+    const OPTION_CONCISE = 'concise';
 
     protected function configure()
     {
@@ -33,6 +35,12 @@ class FindCommand extends Command
                     'The directory containing your media',
                     null
                 ),
+                new InputOption(
+                    self::OPTION_CONCISE,
+                    null,
+                    InputOption::VALUE_NONE,
+                    'Force the output to just list folder names rather than each file'
+                )
             ])
             ->setHelp(<<<EOT
 The <info>$cmd</info> command locates any albums in your specified media directory requiring ID3 tags
@@ -43,11 +51,13 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $needingID3Tags = [];
+        $concise = $input->getOption(self::OPTION_CONCISE);
         foreach (MediaLocator::find($input->getArgument(self::PATH_ARGUMENT), self::FIND_MEDIATYPE) as $mediaEntry) {
             $path = $mediaEntry->getPath();
             $file = new CMP3File($path);
             if ($file->needsID3Info()) {
-                $needingID3Tags[] = $path;
+                $showPath = $concise ? dirname($path) : $path;
+                $needingID3Tags[$showPath] = $showPath; // using path as key prevents duplicate directory paths
             }
         }
         if ($needingID3Tags) {
